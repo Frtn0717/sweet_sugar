0736f82477c77135d43c07d3b34f277998fc5aa2
Stanislau Turasau <pontty232@gmail.com>
2021-09-24 20:12:45 +0300

    fix: correct all notes

--- hometask08/script.js
+++ hometask08/script.js
@@ -1,51 +1,51 @@
 class Entity {
   constructor(name) {
     this.name = name;
-  }
-}
+  };
+};
 
 class Stuff extends Entity {
   constructor(name, color) {
     super(name);
-    this.color = color;
-  }
-}
+    
+    if (color) {
+      this.color = color;
+    };
+  };
+};
 
 class Box extends Entity {
   constructor(name, content, owner) {
     super(name);
-    this.content = content.map(item => {
-      if (item.color) {
-        return {name: item.name, color: item.color}
-      } else {
-        return {name: item.name}
-      }
-    });
+    this.content = content;
     this.ownerName = owner.name;
   };
-
-}
+};
 
 class User extends Entity {
   constructor(name, age, position) {
     super(name);
     this.age = age;
     this.position = position;
-  }
+  };
 
   getAge() {
     return this.age;
-  }
+  };
 
   getPos() {
     return this.position;
-  }
+  };
 
-  setBoxesNames(box) {
-    return this.boxesNames = [box];
-  }
+  setBoxesNames(name) {
+    if (this.boxesNames) {
+      this.boxesNames.push(name);
+    } else {
+      this.boxesNames = [name];
+    };
+  };
 
-}
+};
 
 const user1 = new User('Stan', 33, 'developer');
 const user2 = new User('Michael', 23, 'designer');
@@ -54,7 +54,7 @@ const user3 = new User('Samantha', 27, 'manager');
 const box1 = new Box(
   'Old sneaker box', 
   [new Stuff('pencil', 'grey'), new Stuff('pen', 'blue'), new Stuff('paper', 'white')],
-  new User('Stan', 33, 'developer')
+  user1
   );
 
   user1.setBoxesNames(box1.name);
@@ -62,7 +62,7 @@ const box1 = new Box(
 const box2 = new Box(
   'Box of chocolates', 
   [new Stuff('pencil', 'black'), new Stuff('eraser'), new Stuff('paper', 'white')],
-  new User('Michael', 23, 'designer')
+  user2
   );
 
   user2.setBoxesNames(box2.name);
@@ -70,7 +70,7 @@ const box2 = new Box(
 const box3 = new Box(
   'Handmade box', 
   [new Stuff('pen', 'red'), new Stuff('calculator', 'black'), new Stuff('paper')],
-  new User('Samantha', 27, 'manager')
+  user3
   );
 
   user3.setBoxesNames(box3.name);
@@ -84,19 +84,18 @@ function showInfo(box, user) {
       return ` ${item.color} ${item.name}`;
     } else {
       return ` ${item.name}`;
-    }
+    };
   })}.`
 };
 
-
-  // console.log(box1);
-  // console.log(box2);
-  // console.log(box3);
+  console.log(box1);
+  console.log(box2);
+  console.log(box3);
 
   // console.log(user1);
   // console.log(user2);
   // console.log(user3);
 
-console.log(showInfo(box1, user1));
-console.log(showInfo(box2, user2));
-console.log(showInfo(box3, user3));
+// console.log(showInfo(box1, user1));
+// console.log(showInfo(box2, user2));
+// console.log(showInfo(box3, user3));
